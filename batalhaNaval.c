485b1ae7bd3c8d1efe18a975773f3713dace9951
joaovictor996435642-wq <joaovictor996435642@gmail.com>
2025-11-26 23:24:46 +0000

    Finalização do nivel Novato

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -10,6 +10,58 @@ int main() {
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
 
+    
+
+    int tabuleiro[10][10];// tabuleiro fixo 10x10 
+    int tamanhoNavio = 3; // tamanho fixo do navio
+
+    // 1. Inicializar tudo com 0 
+    for (int i = 0; i < 10; i++) { 
+        for (int j = 0; j < 10; j++) { 
+            tabuleiro[i][j] = 0; 
+        } 
+    }
+
+    // 2. Navios como vetores 
+    int navioHorizontal[3] = {3, 3, 3}; 
+    int navioVertical[3] = {3, 3, 3};
+
+    // Coordenadas iniciais dos navios 
+    int linhaH = 2, colunaH = 1; 
+    int linhaV = 5, colunaV = 7;
+
+    //3. Validação simples do limite do tabuleiro
+    if (colunaH + tamanhoNavio > 10 || linhaV + tamanhoNavio > 10){
+        printf("Erro: um dos navios está fora do tabuleiro.\n"); 
+        return 1;
+    }
+
+    // 4. Posicionar o navio horizontal
+    for (int i = 0; i < tamanhoNavio; i++){
+        tabuleiro[linhaH][colunaH + i] = navioHorizontal[i];
+    }
+
+    // 5. Posicionar o navio vertical
+    for (size_t i = 0; i < tamanhoNavio; i++){
+        tabuleiro[linhaV + i][colunaV] = navioVertical[i];
+    }
+
+    // 6. Exibir o tabuleiro com letras e números
+    printf(" A B C D E F G H I J\n");
+
+    for (size_t i = 0; i < 10; i++)
+    {
+        printf("%2d", i);
+
+        for (size_t j = 0; j < 10; j++)
+        {
+            printf("%d", tabuleiro[i][j]);
+        }
+
+        printf("\n");
+        
+    }
+    
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
     // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
     // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
