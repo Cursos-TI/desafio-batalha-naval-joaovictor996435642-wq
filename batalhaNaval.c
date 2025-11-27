b90f7e5d06887d56c58f4ecdd84e0bd8a16fbab7
joaovictor996435642-wq <joaovictor996435642@gmail.com>
2025-11-27 00:54:07 +0000

    finalizado

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -55,7 +55,7 @@ int main() {
     // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
     // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
 
-    //1.1 Direções
+    /*1.1 Direções
     int dx[4] = {0,1,1,1};  // H, V, DIAG DIR, DIAG ESQ
     int dy[4] = {1,0,1,1};
 
@@ -81,7 +81,7 @@ int main() {
 
     for (size_t i = 0; i < 10; i++)
     {
-        printf("%2d", i);
+        printf("%d", i);
 
         for (size_t j = 0; j < 10; j++)
         {
@@ -92,7 +92,7 @@ int main() {
         
     }
     
-    
+    */
 
 
     // Nível Mestre - Habilidades Especiais com Matrizes
@@ -116,7 +116,115 @@ int main() {
     // 1 1 1 1 1
     // 0 0 1 0 0
 
-    
+    // 1. POSICIONAR NAVIOS
 
+    int h_l = 2,h_c = 1;
+    int v_l = 5, v_c = 7;
+    int d1_l = 0, d1_c = 0;
+    int d2_l = 0, d2_c = 9;
+
+    for (int i = 0; i < tamanho; i++){
+        tabuleiro[h_l][h_c + i]= 3;
+    }
+
+    for (int i = 0; i < tamanho; i++){
+        tabuleiro[v_l + i][v_c]= 3;
+    }
+
+    for (int i = 0; i < tamanho; i++){
+        tabuleiro[d1_l + i][d1_c + i]= 3;
+    }
+
+    for (int i = 0; i < tamanho; i++){
+        tabuleiro[d2_l + i][d2_c - i]= 3;
+    }
+
+    // 2. MATRIZES DE HABILIDADES (FORMATO DO DESAFIO)
+
+    int H = 3; //altura
+    int L = 5; // largura
+
+    int cone[3][5]= {
+        {0,0,1,0,0},
+        {0,1,1,1,0},
+        {1,1,1,1,1}
+    };
+
+    int cruz[3][5] = {
+        {0,0,1,0,0},
+        {1,1,1,1,1},
+        {0,0,1,0,0}
+    };
+
+    int octa[3][5] = {
+      {0,0,1,0,0},
+      {0,1,1,1,0},
+      {0,0,1,0,0}  
+    };
+
+    ///3. CENTROS DAS HABILIDADES
+
+    int coneL = 4, coneC = 4;
+    int cruzL = 7, cruzC = 2;
+    int octaL = 3, octaC = 7;
+
+    int centroL = H / 2;  // linha 1
+    int centroC = L / 2;  // coluna 2
+
+    // 4. SOBREPOR HABILIDADES NO TABULEIRO
+
+    for (int i = 0; i < H; i++){
+        for (int j = 0; j < L; j++){
+            if (cone[i][j] == 1){
+                int TL = coneL + (i - centroL);
+                int TC = coneC + (j - centroC);
+                if(TL >= 0 && TL < 10 && TC >= 0 && TC < 10)
+                tabuleiro[TL][TC] = 5;
+            }
+        }
+        
+    }
+
+    for (int i = 0; i < H; i++){
+        for (int j = 0; j < L; j++){
+            if(cone[i][j] == 1){
+                int TL = cruzL + (i - centroL);
+                int TC = cruzC + (j - centroC);
+                if(TL >= 0 && TL < 10 && TC >= 0 && TC < 10)
+                tabuleiro[TL][TC] = 5;
+            }
+        }
+        
+    }
+
+    for (int i = 0; i < H; i++){
+        for (int j = 0; j < L; j++){
+            if(cone[i][j] == 1){
+                int TL = octaL + (i - centroL);
+                int TC = octaC + (j - centroC);
+                if(TL >= 0 && TL < 10 && TC >= 0 && TC < 10)
+                tabuleiro[TL][TC] = 5;
+            }
+        }
+        
+    }
+
+    // 5. EXIBIR TABULEIRO FINAL
+
+    printf(" A B C D E F G H I J\n");
+
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d", i);
+
+        for (int j = 0; j < 10; j++)
+        {
+            printf("%d", tabuleiro[i][j]);
+        }
+
+        printf("\n");
+        
+    }
+    
     return 0;
 }
